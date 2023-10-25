823000660c8130722751417672ecb34c9c8f8f2c
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-25 19:00:26 +0300

    first master commit

---
+++ PR 5.5/PR 5.5.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int d = 0;
+int max_d = 0;
+
+int A(int m, int n) {
+    d++;
+    if (d > max_d) {
+        max_d = d;
+    }
+    int result;
+    if (m == 0) {
+        result = n + 1;
+    }
+    else if (n == 0) {
+        result = A(m - 1, 1);
+    }
+    else {
+        result = A(m - 1, A(m, n - 1));
+    }
+    d--;
+    return result;
+}
+
+int main() {
+    int m, n;
+    cout << "m = ";
+    cin >> m;
+    cout << "n = ";
+    cin >> n;
+
+    int result = A(m, n);
+    cout << "The value of the Ackerman function: " << result << endl;
+    cout << "Maximum recursion depth: " << max_d << endl;
+
+    return 0;
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR 5.5/PR 5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(AckermannFunctionTest) {
+public:
+
+    TEST_METHOD(A_2_3_Returns_9) {
+        int result = A(2, 3);
+        Assert::AreEqual(9, result);
+    }
+};
